57ae7d8877be803db860f26e4c9891f8c643a9aa
moisesHerrera01 <moises.oct@gmail.com>
2017-05-20 21:48:53 -0600

    config
    configura base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sicbaf/';
+$config['base_url'] = 'http://localhost/sigb/';
 
 /*
 |--------------------------------------------------------------------------
