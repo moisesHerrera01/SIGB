174488a5ed52058fd8b0fc0feb24d2c8ac5276f3
Walter Lemus <walter.lemus@hotmail.com>
2017-06-04 23:15:37 -0600

    archivos para reportes por proveedor, factura y saldo

---
+++ application/controllers/Tactico/Proveedor_factura_saldos.php
@@ -0,0 +1,316 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Proveedor_factura_saldos extends CI_Controller {
+  public function RecibirFiltro() {
+
+    if ($this->input->post()==NULL) {
+      redirect('Bodega/Proveedores/Reporte/');
+    } else {
+      redirect('Bodega/Proveedores/Reporte/'.$this->input->post('fuente').'/'.$this->input->post('fechaMin').'/'.$this->input->post('fechaMax') );
+    }
+  }
+
+  public function Reporte(){
+    $this->load->model('Bodega/Fuentefondos_model');
+    $USER = $this->session->userdata('logged_in');
+    if($USER){
+      $this->load->library(array('table'));
+
+      $data['title'] = "4-Reporte por Proveedor, Factura y Especifico";
+      $data['menu'] = $this->menu_dinamico->menus($this->session->userdata('logged_in'),$this->uri->segment(1));
+      $data['js'] = 'assets/js/validate/reporte/bodega/general.js';
+
+      $template = array(
+          'table_open' => '<table class="table table-striped table-bordered">'
+      );
+      $this->table->set_template($template);
+      $this->table->set_heading('Fecha Factura', 'Numero Factura', 'Compromiso', 'Proveedor', 'Objeto Especifico',
+                                'Total OE');
+
+      $num = 12;
+      $total_registros = $this->Proveedor->TotalReporteProveedores($this->uri->segment(4), $this->uri->segment(5), $this->uri->segment(6));
+      $pagination = paginacion('index.php/Bodega/Proveedores/Reporte/' .$this->uri->segment(4). '/' .$this->uri->segment(5). '/' . $this->uri->segment(6),
+                    $total_registros, $num, '7');
+
+      if ($total_registros > 0) {
+
+        $registros = $this->Proveedor->ReporteProveedores($this->uri->segment(4), $this->uri->segment(5), $this->uri->segment(6), $num, $this->uri->segment(7));
+
+        $total = 0;
+        while ($registro = current($registros)) {
+          $this->table->add_row($registro['fecha_factura'], $registro['numero_factura'], $registro['numero_compromiso'],
+                                $registro['nombre_proveedor'], $registro['id_especifico'], number_format($registro['total'], 3));
+
+          $total += $registro['total'];
+
+          $next = next($registros);
+
+          if ($next != FALSE) {
+            if($registro['id_factura'] != $next['id_factura'] && $total != 0){
+              $msg = array('data' => "Total factura:", 'colspan' => "5");
+              $this->table->add_row($msg,  number_format($total, 3));
+              $total = 0;
+            }
+          } else {
+            $msg = array('data' => "Total factura:", 'colspan' => "5");
+            $this->table->add_row($msg,  number_format($total, 3));
+            $total = 0;
+          }
+
+        }
+      } else {
+        $msg = array('data' => "No se encontraron resultados", 'colspan' => "6");
+        $this->table->add_row($msg);
+      }
+
+      $data['body'] = $this->load->view('Bodega/Reportes/filtro_fuentes_view', array('url' => '/Bodega/Proveedores/RecibirFiltro','title'=>$data['title']) ,TRUE) . "<br>" .
+                      "<div class='content_table'>" .
+                      "<div class='limit-content-title'><span class='icono icon-table icon-title'> ".$this->Fuentefondos_model->obtenerFuente($this->uri->segment(4)). " " . $this->uri->segment(5) ." - ". $this->uri->segment(6) ."</span></div>".
+                      "<div class='limit-content'>" .
+                     "<div class='exportar'>
+                        <a href='".base_url('/index.php/Bodega/Proveedores/ReporteExcel/'.$this->uri->segment(4).'/'.$this->uri->segment(5).'/'.$this->uri->segment(6))."'
+                        class='icono icon-file-excel'> Exportar Excel</a>
+                     </div>" . "<div class='table-responsive'>" . $this->table->generate() . "</div>" . $pagination . "</div></div>";
+
+      $this->load->view('base', $data);
+    } else {
+      redirect('login/index/forbidden');
+    }
+  }
+
+  public function ReporteExcel() {
+    $this->load->library(array('excel'));
+    $estilo_titulo = array(
+        'font' => array(
+          'name' => 'Calibri',
+          'bold' => TRUE,
+          'size' => 12,
+        ),
+        'borders' => array(
+          'allborders' => array(
+          'style' => PHPExcel_Style_Border::BORDER_THICK
+          ),
+  		    'color' => array('rgb' => '676767'),
+        ),
+  	    'alignment' => array(
+          'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+          'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER,
+          'rotation' => 0,
+          'wrap' => TRUE,
+        ),
+    );
+
+    $estilo_contenido = array(
+      'font' => array(
+        'name' => 'Calibri',
+        'bold' => FALSE,
+        'size' => 11,
+      ),
+      'borders' => array(
+        'allborders' => array(
+        'style' => PHPExcel_Style_Border::BORDER_THIN
+        ),
+		    'color' => array('rgb' => '676767'),
+      ),
+      'alignment' => array(
+        'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+        'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER,
+        'rotation' => 0,
+        'wrap' => TRUE,
+      ),
+    );
+
+    $objPHPExcel = new PHPExcel();
+    $objPHPExcel->getProperties()->setCreator("SICBAF")
+    						 ->setLastModifiedBy("SICBAF")
+    						 ->setTitle("Reporte por Proveedor, Factura y Especifico")
+    						 ->setSubject("Reporte por Proveedor, Factura y Especifico")
+    						 ->setDescription("Reporte por Proveedor, Factura y Especifico.")
+    						 ->setKeywords("office PHPExcel php")
+    						 ->setCategory("Reporte por Proveedor, Factura y Especifico");
+    $objPHPExcel->setActiveSheetIndex(0)
+                 ->setCellValue('A1', 'Fecha Factura')
+                 ->setCellValue('B1', 'Numero Factura')
+                 ->setCellValue('C1', 'Compromiso')
+                 ->setCellValue('D1', 'Proveedor')
+                 ->setCellValue('E1', 'Objeto Especifico')
+                 ->setCellValue('F1', ' Total OE');
+    $objPHPExcel->getActiveSheet()->getStyle('A1:F1')->applyFromArray($estilo_titulo);
+
+    $total_registros = $this->Proveedor->TotalReporteProveedores($this->uri->segment(4), $this->uri->segment(5), $this->uri->segment(6));
+
+    if ($total_registros > 0) {
+
+      $registros = $this->Proveedor->ReporteProveedores($this->uri->segment(4), $this->uri->segment(5), $this->uri->segment(6));
+
+      $total = 0;
+      $i = 2;
+      while ($registro = current($registros)) {
+
+        $objPHPExcel->setActiveSheetIndex(0)
+                    ->setCellValue('A'.$i, $registro['fecha_factura'])
+                    ->setCellValue('B'.$i, $registro['numero_factura'])
+                    ->setCellValue('C'.$i, $registro['numero_compromiso'])
+                    ->setCellValue('D'.$i, $registro['nombre_proveedor'])
+                    ->setCellValue('E'.$i, $registro['id_especifico'])
+                    ->setCellValue('F'.$i, $registro['total']);
+        $objPHPExcel->getActiveSheet()->getStyle('A'.$i.':f'.$i)->applyFromArray($estilo_contenido);
+
+        $total += $registro['total'];
+
+        $next = next($registros);
+
+        if ($next != FALSE) {
+          if($registro['id_factura'] != $next['id_factura'] && $total != 0){
+            $i++;
+            $objPHPExcel->setActiveSheetIndex(0)->mergeCells('A'.$i.':E'.$i);
+            $objPHPExcel->setActiveSheetIndex(0)
+                        ->setCellValue('A'.$i, "Total factura:")
+                        ->setCellValue('F'.$i, $total);
+            $total = 0;
+            $objPHPExcel->getActiveSheet()->getStyle('A'.$i.':f'.$i)->applyFromArray($estilo_contenido);
+          }
+        } else {
+          $i++;
+          $objPHPExcel->setActiveSheetIndex(0)->mergeCells('A'.$i.':E'.$i);
+          $objPHPExcel->setActiveSheetIndex(0)
+                      ->setCellValue('A'.$i, "Total factura:")
+                      ->setCellValue('F'.$i, $total);
+          $total = 0;
+          $objPHPExcel->getActiveSheet()->getStyle('A'.$i.':f'.$i)->applyFromArray($estilo_contenido);
+        }
+        $i++;
+      }
+    } else {
+      $objPHPExcel->setActiveSheetIndex(0)->mergeCells('A2:F2');
+      $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A2', "No se encontraron resultados");
+      $objPHPExcel->getActiveSheet()->getStyle('A2:f2')->applyFromArray($estilo_contenido);
+    }
+
+    foreach(range('A','F') as $columnID){
+      $objPHPExcel->getActiveSheet()->getColumnDimension($columnID)->setAutoSize(true);
+    }
+
+    $objPHPExcel->setActiveSheetIndex(0);
+
+    header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+    header("Content-Disposition: attachment; filename='reporte_proveedores.xlsx'");
+    header('Cache-Control: max-age=0');
+
+    $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+    $objWriter->save('php://output');
+  }
+
+
+  public function ReporteExcelProveedores() {
+    $USER = $this->session->userdata('logged_in');
+    if($USER){
+      $this->load->library(array('excel'));
+
+      $estilo_titulo = array(
+        'font' => array(
+          'name' => 'Calibri',
+          'bold' => TRUE,
+          'size' => 12,
+        ),
+        'borders' => array(
+          'allborders' => array(
+          'style' => PHPExcel_Style_Border::BORDER_THICK
+          ),
+          'color' => array('rgb' => '676767'),
+        ),
+        'alignment' => array(
+          'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+          'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER,
+          'rotation' => 0,
+          'wrap' => TRUE,
+        ),
+      );
+
+
+      $estilo_contenido = array(
+        'font' => array(
+          'name' => 'Calibri',
+          'bold' => FALSE,
+          'size' => 11,
+        ),
+        'borders' => array(
+          'allborders' => array(
+          'style' => PHPExcel_Style_Border::BORDER_THIN
+          ),
+          'color' => array('rgb' => '676767'),
+        ),
+        'alignment' => array(
+          'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+          'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER,
+          'rotation' => 0,
+          'wrap' => TRUE,
+        ),
+      );
+
+      $objPHPExcel = new PHPExcel();
+      $objPHPExcel->getProperties()->setCreator("SICBAF")
+                   ->setLastModifiedBy("SICBAF")
+                   ->setTitle("Reporte de proveedores.")
+                   ->setSubject("Reporte de proveedores.")
+                   ->setDescription("Reporte de proveedores. ")
+                   ->setKeywords("office PHPExcel php")
+                   ->setCategory("Reporte de proveedores.");
+
+      $objPHPExcel->setActiveSheetIndex(0)
+                   ->setCellValue('A1', 'ID')
+                   ->setCellValue('B1', 'Nombre proveedor')
+                   ->setCellValue('C1', 'Nombre contacto')
+                   ->setCellValue('D1', 'NIT')
+                   ->setCellValue('E1', 'Correo Electrónico')
+                   ->setCellValue('F1', 'Teléfono')
+                   ->setCellValue('G1', 'Fax')
+                   ->setCellValue('H1', 'Dirección')
+                   ->setCellValue('I1', 'Categoría')
+                   ->setCellValue('J1', 'Tipo Servicio')
+                   ->setCellValue('K1', 'Tipo Empresa');
+
+      $objPHPExcel->getActiveSheet()->getStyle('A1:K1')->applyFromArray($estilo_titulo);
+
+      $registros = $this->Proveedor->obtenerProveedores();
+      if (!($registros == FALSE)) {
+        $i = 2;
+        foreach($registros as $pro) {
+          $objPHPExcel->setActiveSheetIndex(0)
+                      ->setCellValue('A'.$i, $pro->id_proveedores)
+                      ->setCellValue('B'.$i, $pro->nombre_proveedor)
+                      ->setCellValue('C'.$i, $pro->nombre_contacto)
+                      ->setCellValue('D'.$i, $pro->nit)
+                      ->setCellValue('E'.$i, $pro->correo)
+                      ->setCellValue('F'.$i, $pro->telefono)
+                      ->setCellValue('G'.$i, $pro->fax)
+                      ->setCellValue('H'.$i, $pro->direccion)
+                      ->setCellValue('I'.$i, $pro->nombre_categoria)
+                      ->setCellValue('J'.$i, $pro->rubro)
+                      ->setCellValue('K'.$i, $pro->tipo_empresa);
+
+          $objPHPExcel->getActiveSheet()->getStyle('A'.$i.':K'.$i)->applyFromArray($estilo_contenido);
+          $i++;
+        }
+
+        foreach(range('A','K') as $columnID){
+          $objPHPExcel->getActiveSheet()->getColumnDimension($columnID)->setAutoSize(true);
+        }
+
+        $objPHPExcel->setActiveSheetIndex(0);
+
+        header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        header("Content-Disposition: attachment; filename='reporte_proveedores.xlsx'");
+        header('Cache-Control: max-age=0');
+
+        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+        $objWriter->save('php://output');
+      }
+    } else {
+      redirect('login');
+    }
+  }
+}
+
+?>
---
+++ application/views/Tactico/filtro_fuentes_view.php
@@ -0,0 +1,70 @@
+<?php
+
+echo $this->breadcrumb->build_breadcrump(implode('/', array_slice($this->uri->segment_array(), 0, 3)));
+
+echo "<div style='text-align:center'>";
+    echo "<div class='form-group'>";
+    echo "<h3><font color=black>$title</font></h3>";
+  echo "</div>";
+echo "</div>";
+
+$fuente = array(
+    'name' => 'autocomplete1',
+    'placeholder' => 'Escribe Fuente de Fondos',
+    'class' => "form-control",
+    'autocomplete' => 'off'
+);
+
+$fechaInicial = array(
+    'name' => 'fechaMin',
+    'type' => "date",
+    'placeholder' => 'Escribe Fecha de Inicial',
+    'class' => "form-control"
+);
+
+$fechaFinal = array(
+    'name' => 'fechaMax',
+    'type' => "date",
+    'placeholder' => 'Escribe Fecha de Final',
+    'class' => "form-control"
+);
+
+$button = array('class' => 'btn btn-success',);
+$atriLabel = array('class' => 'col-lg-2 control-label');
+
+$atributos = array(
+  'class' => 'form-horizontal',
+  'role' => 'form',
+);
+
+echo "<div class='content-form'>";
+  echo "<div class='limit-content-title'>";
+    echo "<span class='icono icon-filter icon-title'> Filtro</span>";
+  echo "</div>";
+  echo "<div class='limit-content'>";
+    echo form_open($url, $atributos);
+      echo "<div class='form-group'>";
+        echo form_label('Fuente de Fondos:', 'f', $atriLabel);
+        echo "<div class='col-lg-10'>";
+          echo form_input($fuente);
+          echo form_hidden('fuente');
+          echo '<div id="suggestions" class="suggestions"></div>';
+        echo "</div>";
+      echo "</div>";
+      echo "<div class='form-group'>";
+        echo form_label('Fecha inicial:', 'fechaini', $atriLabel);
+        echo "<div class='col-lg-10'>";
+          echo form_input($fechaInicial);
+        echo "</div>";
+      echo "</div>";
+      echo "<div class='form-group'>";
+        echo form_label('Fecha Final:', 'fechafin', $atriLabel);
+        echo "<div class='col-lg-10'>";
+          echo form_input($fechaFinal);
+        echo "</div>";
+      echo "</div>";
+      echo form_submit('','Generar', $button);
+    echo form_close();
+  echo "</div>";
+echo "</div>";
+?>
