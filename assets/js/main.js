3ed38bf38d5b05899a5673b16c5155316e3647f9
Alberto S. Castaneda <alb2594@gmail.com>
2017-06-14 22:55:25 -0600

    cambios colores

--- assets/js/main.js
+++ assets/js/main.js
@@ -206,6 +206,18 @@ $(document).ready(function(){
     $(".close").click(function(){
         $("#myAlert").slideUp();
     });
+
+    $("form").keypress(function(e){
+      if(e == 13){
+        return false;
+      }
+    });
+
+    $('input').keypress(function(e){
+      if(e.which == 13){
+        return false;
+      }
+    });
 });
 
 /*FUNCIONES*/
